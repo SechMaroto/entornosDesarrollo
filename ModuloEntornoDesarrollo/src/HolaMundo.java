0c78e87324dc4887a8ce0e83017db90089f7ba52
Pablo <lovillosolano@gmail.com>
2025-02-14 10:15:22 +0100

    Update HolaMundo.java
    14/02/2025 - v.0.0.0 - nombrecompañero - Subo comentario aclaratorio del código

--- ModuloEntornoDesarrollo/src/HolaMundo.java
+++ ModuloEntornoDesarrollo/src/HolaMundo.java
@@ -5,6 +5,7 @@ public class HolaMundo {
 		
 		System.out.println("¡Hola, mundo!");
 		System.out.println("Hola mundo nueva versión");
+		// Este es un comentario añadido por mi compañero de código
 		
 
 	}
